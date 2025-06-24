1303dca8c18dcd0bf8369481f22faf058eb4a55e
chris taylor <chris.taylor@saronic.com>
2025-06-24 20:45:25 +0000

    set up for mlf

--- finetune.sh
+++ finetune.sh
@@ -1,1 +1,1 @@
-torchrun --master_port=7777 --nproc_per_node=2 train.py -c configs/dfine/custom/dfine_hgnetv2_m_custom.yml --use-amp --seed=0 -t dfine_m_obj365.pth
+torchrun --master_port=7777 --nproc_per_node=8 train.py -c configs/dfine/custom/dfine_hgnetv2_m_custom.yml --use-amp --seed=7289345 -t dfine_m_obj365.pth
